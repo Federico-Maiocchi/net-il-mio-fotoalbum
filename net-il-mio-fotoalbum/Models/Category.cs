03ba4e4686a21c4b2074247c39453fc9605c08c8
Federico Maiocchi <maiocchifederico@gmail.com>
2024-05-27 10:53:41 +0200

    ADD: Photo/ Category class

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("category_name")]
+        public string Name { get; set; }
+
+        public List<Photo> Photos { get; set; }
+
+        //costruttore - vuoto
+        public Category() { }
+
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("Photo")]
+
+    [Index(nameof(Title), IsUnique = true)]
+    public class Photo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("photo_title")]
+        public string Title { get; set; }
+
+        [Column("photo_description")]
+        public string Description { get; set; }
+
+        public byte[]? ImageFile { get; set; }
+
+        public string ImgSrc => ImageFile != null ? $"data:image/png;base64,{Convert.ToBase64String(ImageFile)}" : "";
+
+        public bool IsVisible { get; set; }
+
+        //relazione N : N con CATEGORY
+        public List<Category>? Categories { get; set; }
+
+        //costruttore - vuoto
+        public Photo() { }  
+
+
+    }
+}
